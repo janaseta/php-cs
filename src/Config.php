f9f581649c18c09502d55dff1131d05c4283a4d5
Džuris <juris@glaive.pro>
2022-02-14 20:16:46 +0200

    Add JS Config

---
+++ src/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace JanaSeta\PhpCs;
+
+class Config extends \PhpCsFixer\Config
+{
+	protected array $rules = [
+		'@Symfony:risky' => true,
+		'ternary_to_null_coalescing' => true,
+		'phpdoc_align' => false,
+		'braces' => false,
+		'new_with_braces' => false,
+		'indentation_type' => true,
+	];
+
+	protected $indent = "\t";
+
+	protected $isRiskyAllowed = true;
+
+	protected $lineEnding = "\n";
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->setIndent($this->indent)
+			->setRiskyAllowed($this->isRiskyAllowed)
+			->setLineEnding($this->lineEnding)
+			->setRules($this->rules);
+	}
+}
